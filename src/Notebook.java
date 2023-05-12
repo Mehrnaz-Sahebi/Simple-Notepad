c634d62847fe0f953dcf128b9e6cc544052e603d
Mehrnaz Sahebi <mehrnazsa83@gmail.com>
2023-05-12 18:30:43 +0330

    feat : fields and constructor for class Notebook

--- src/Notebook.java
+++ src/Notebook.java
@@ -1,2 +1,10 @@
 public class Notebook {
+    private String notebookFileName;
+
+    public Notebook(String notebookFileName) {
+        StringBuilder stringBuilder = new StringBuilder("./");
+        stringBuilder.append(notebookFileName);
+        stringBuilder.append(".bin");
+        this.notebookFileName = stringBuilder.toString();
+    }
 }
