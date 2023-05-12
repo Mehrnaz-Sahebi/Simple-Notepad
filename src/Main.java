cc4c21178abae92ae1700db6f3b7dfa51bfe80d4
Mehrnaz Sahebi <mehrnazsa83@gmail.com>
2023-05-12 18:41:33 +0330

    feat : main method in Main

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,11 @@
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the name of the notebook (for example : myNotebook)");
+        String notebookName = scanner.nextLine();
+        Notebook notebook = new Notebook(notebookName);
+        Menu.firstPage(notebook);
+    }
 }
